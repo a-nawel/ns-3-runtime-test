9a893d10649d4cd0f904178ec064dfb349eb290e
Nawel <nawel.alioua@gmail.com>
2021-10-30 07:25:49 +0900

    Create a new ns-3 module to host all our code in one place This directory tree represents the basic template of any ns-3 module. We probably won't need to work on all those directories.

---
+++ model/ns-3-runtime-test.h
@@ -0,0 +1,12 @@
+/* -*- Mode:C++; c-file-style:"gnu"; indent-tabs-mode:nil; -*- */
+#ifndef NS_3_RUNTIME_TEST_H
+#define NS_3_RUNTIME_TEST_H
+
+namespace ns3 {
+
+/* ... */
+
+}
+
+#endif /* NS_3_RUNTIME_TEST_H */
+
